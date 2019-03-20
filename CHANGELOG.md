84bf3c0536ca9be6a406de73eee739535bb4e46b
Puja Pramudya <puja@radyalabs.com>
2019-03-20 16:06:25 +0700

    [puja/gio] setup CI/CD

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+**6.4.1-1**
+
+- Start proper versioning of the cookbook
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'baritolog@go-jek.com'
 license 'Apache-2.0'
 description 'Installs/Configures elasticsearch_wrapper_cookbook'
 long_description 'Installs/Configures elasticsearch_wrapper_cookbook'
-version '0.1.0'
+version '6.4.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/BaritoLog/elasticsearch_wrapper_cookbook/issues'
