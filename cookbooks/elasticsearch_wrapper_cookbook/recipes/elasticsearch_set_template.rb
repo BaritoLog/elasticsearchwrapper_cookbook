9d433e7aaa387b0cf1b6a679166539d2caf94c65
Galuh Estya <galuhestya@gmail.com>
2020-07-22 09:37:02 +0700

    7.5.0 update cookbook

--- cookbooks/elasticsearch_wrapper_cookbook/recipes/elasticsearch_set_template.rb
+++ cookbooks/elasticsearch_wrapper_cookbook/recipes/elasticsearch_set_template.rb
@@ -5,27 +5,46 @@
 # Copyright:: 2018, BaritoLog.
 #
 #
+require 'json'
 version = node['elasticsearch']['version']
 ipaddress = node['ipaddress']
+xpack_enabled = node['elasticsearch']['security']['xpack_security_enabled']
+bootstrap_password = Base64.decode64(node['elasticsearch']['security']['bootstrap_password'])
+override_base_template = node['elasticsearch']['override_base_template']
+basic_auth = Base64.encode64("elastic:#{bootstrap_password}")
 
-if version >= '6.0.0' && version < '7.0.0'
-  base_template = defined?(node['elasticsearch']['base_template']) ? node['elasticsearch']['base_template'] : node['elasticsearch']['base_template_es6']
+if override_base_template
+  base_template = node['elasticsearch']['base_template']
+elsif version >= '6.0.0' && version < '7.0.0'
+  base_template = node['elasticsearch']['base_template_es6']
 elsif version >= '7.0.0' && version < '8.0.0'
-  base_template = defined?(node['elasticsearch']['base_template']) ? node['elasticsearch']['base_template'] : node['elasticsearch']['base_template_es7']
+  base_template = node['elasticsearch']['base_template_es7']
 else
   base_template = node['elasticsearch']['base_template']
 end
 
 port = node['elasticsearch']['port']
 
-# Since ES >= 5, index configuration cannot using yaml file, using dynamic config API instead
-
-# Create default template which will used by future indices with default number_of_replicas
-http_request 'Create base template' do
-  url "http://#{ipaddress}:#{port}/_template/base_default"
-  action :put
-  headers "Content-Type" => "application/json"
-  message "#{base_template}"
-  retry_delay 30
-  retries 10
+if xpack_enabled
+  http_request 'Create base template' do
+    url "http://#{ipaddress}:#{port}/_template/base_template"
+    action :put
+    headers({'AUTHORIZATION' => "Basic #{basic_auth}",
+      'Content-Type' => 'application/json'
+    })
+    message(base_template.to_json)
+    ignore_failure true
+    retry_delay 30
+    retries 10
+  end
+else
+  http_request 'Create base template' do
+    url "http://#{ipaddress}:#{port}/_template/base_template"
+    action :put
+    headers 'Content-Type' => 'application/json'
+    message(base_template.to_json)
+    ignore_failure true
+    retry_delay 30
+    retries 10
+  end
 end
