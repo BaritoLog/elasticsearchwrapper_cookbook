7045bf04445d517210b5f163d0e6a59d36cc2962
Galuh Estya <galuhestya@gmail.com>
2020-08-04 16:58:11 +0700

    update elasticsearch to 7.5.2 by default instead of 7.5.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,7 +10,7 @@
 default['elasticsearch']['user'] = 'elasticsearch'
 
 # Elasticsearch configuration
-default['elasticsearch']['version'] = '7.5.0'
+default['elasticsearch']['version'] = '7.5.2'
 default['elasticsearch']['port'] = 9200
 default['elasticsearch']['auto_create_index'] = true
 default['elasticsearch']['data_directory'] = '/var/lib/elasticsearch'
