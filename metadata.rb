e5fce667a3584d352330f0430227934f72658d9f
Galuh Estya <galuhestya@gmail.com>
2020-08-05 18:08:15 +0700

    don't run elasticsearch_api on image creation

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'baritolog@go-jek.com'
 license 'Apache-2.0'
 description 'Installs/Configures elasticsearch_wrapper_cookbook'
 long_description 'Installs/Configures elasticsearch_wrapper_cookbook'
-version '7.5.2'
+version '7.5.3'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/BaritoLog/elasticsearch_wrapper_cookbook/issues'
